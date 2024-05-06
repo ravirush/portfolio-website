58d890077482eb56bb2a41e3db4e0fa98f363c8e
Ravindu Aththanayaka <ravindurashmika2k@gmail.com>
2024-05-06 19:35:48 +0530

    contact section added

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,9 +7,14 @@ import Link from "next/link";
 import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
   const {ref} = useSectionInView("Home", 0.5);
+  const {
+    setActiveSection,
+    setTimeOfLastClick,
+  } = useActiveSectionContext();
 
   return (
     <section ref={ref} id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
@@ -52,7 +57,12 @@ export default function Intro() {
         transition={{
           delay: 0.1,
         }}>
-          <Link href="#contact" className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">
+          <Link href="#contact" className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+            onClick={() => {
+              setActiveSection("Contact");
+              setTimeOfLastClick(Date.now());
+            }}
+          >
             Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" /></Link>
 
           <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" href="/CV.pdf" download>
