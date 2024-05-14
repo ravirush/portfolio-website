689bc9f5ca77988e91327dd2baed2145c7c31e49
Ravindu Aththanayaka <ravindurashmika2k@gmail.com>
2024-05-14 18:25:33 +0530

    contact me update

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,57 +1,57 @@
-"use client";
+'use client';
+import { sendEmail } from '@/actions/sendEmail';
+import { useSectionInView } from '@/lib/hooks';
+import { motion } from 'framer-motion'
+import React from 'react'
+import SubmitBtn from './submit-btn';
+import toast from 'react-hot-toast';
 
-import React from "react";
-import SectionHeading from "./section-heading";
-import { motion } from "framer-motion";
-import { useSectionInView } from "@/lib/hooks";
-import { sendEmail } from "@/actions/sendEmail";
-import SubmitBtn from "./submit-btn";
-import toast from "react-hot-toast";
 
 export default function Contact() {
-    const { ref } = useSectionInView("Contact");
+  const {ref} = useSectionInView('Contact', 0.9);
 
-    return (
-        <motion.section ref={ref} id="contact" className="mb-20 sm:mb-28 w-[min(100%,40rem)] text-center" 
-            initial={{opacity:0,}} 
-            whileInView={{opacity: 1,}} 
-            transition={{duration: 1}} 
-            viewport={{once: true,}}
-        >
-            <SectionHeading>Contact Me</SectionHeading>
+  
 
-            <p className="text-gray-700 -mt-6 dark:text-white/80">Please contact me directly at{" "} <a className="underline" href="mailto:ravindurashmika2k@gmail.com">ravindurashmika2k@gmail.com</a>{" "} or through this form.</p>
-            
-            <form className="mt-10 flex flex-col dark:text-black"
-                action={async (formData) => {
-                    const { data, error } = await sendEmail(formData);
+  return (
+    <motion.section ref = {ref} id='contact' className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center' 
+    initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 1}} viewport={{once: true}}>
+      <h2 className='text-3xl font-medium mb-8'>Contact me</h2>
 
-                    if (error) {
-                        toast.error(error);
-                        return;
-                    }
-                    
-                    toast.success("Email sent successfully!");
-                }}
-            >
-                <input 
-                    className="h-14 rounded-lg borderBlack px-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-                    name="senderEmail" 
-                    type="email" 
-                    required 
-                    maxLength={500} 
-                    placeholder="Your email" 
-                />
-                <textarea 
-                    className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-                    name="message" 
-                    placeholder="Your message" 
-                    required 
-                    maxLength={5000} 
-                />
-                <SubmitBtn />
-            </form>
+      <p className='text-gray-700 -mt-5 dark:text-white/80'>Feel free to contact me at {" "}
+        <a className='underline' href='mailto:ravindurashmika2k@gmail.com'>ravindurashmika2k@gmail.com</a> {" "}
+        or through this form.
+      </p>
 
-        </motion.section>
-    );
+      <form className='mt-10 flex flex-col dark:text-black' 
+        action={async (formData) => {
+          const {data, error} = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success('Email sent successfully!');
+        }}
+      >
+        <input 
+          type="email" 
+          name='senderEmail'
+          required maxLength={500} 
+          placeholder='Your email' 
+          className='px-4 h-14 rounded-lg border border-black/10 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none' 
+        />
+
+        <textarea 
+          required 
+          name='message' 
+          maxLength={5000} 
+          placeholder='Your message' 
+          className='h-52 my-3 rounded-lg border border-black/10 p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none' 
+        />
+
+        <SubmitBtn />
+      </form>
+    </motion.section>
+  )
 }
--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   Html,
   Body,
@@ -8,37 +8,33 @@ import {
   Container,
   Preview,
   Section,
-  Text,
-} from "@react-email/components";
-import { Tailwind } from "@react-email/tailwind";
+  Text
+} from '@react-email/components'
+import { Tailwind } from '@react-email/tailwind'
 
 type ContactFormEmailProps = {
   message: string;
   senderEmail: string;
-};
+}
 
 export default function ContactFormEmail({
   message,
-  senderEmail,
+  senderEmail
 }: ContactFormEmailProps) {
-  return (
-    <Html>
-      <Head />
-      <Preview>New message from your portfolio site</Preview>
-      <Tailwind>
-        <Body className="bg-gray-100 text-black">
-          <Container>
-            <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md">
-              <Heading className="leading-tight">
-                You received the following message from the contact form
-              </Heading>
-              <Text>{message}</Text>
-              <Hr />
-              <Text>The sender's email is: {senderEmail}</Text>
-            </Section>
-          </Container>
-        </Body>
-      </Tailwind>
-    </Html>
-  );
+  return <Html>
+    <Head />
+    <Preview>New message from portfolio</Preview>
+    <Tailwind>
+      <Body className='bg-gray-100'>
+        <Container>
+          <Section className='bg-white border border-black/[0.1] my-10 px-10 py-4 rounded-md'>
+            <Heading className='leading-tight text-black'>You received the following message from the contact form</Heading>
+            <Text>{message}</Text>
+            <Hr />
+            <Text>The sender's email is: {senderEmail}</Text>
+          </Section>
+        </Container>
+      </Body>
+    </Tailwind>
+  </Html>
 }
