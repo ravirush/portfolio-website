7a081a9e58f8c8bad45f1c59f413a6e60a2f7d6d
Ravindu Aththanayaka <ravindurashmika2k@gmail.com>
2024-05-14 11:19:45 +0530

    intro update

--- components/intro.tsx
+++ components/intro.tsx
@@ -47,8 +47,8 @@ export default function Intro() {
 
         <motion.h1 className='relative mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl' 
         initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}}>
-        <span className='font-bold'>Hello, I'm Ravindu Aththanayaka. </span>
-        <span>I'm a dedicated {" "} <span className='font-bold'>Undergraduate </span>with a strong interest in <span className='font-bold'>Software Development</span>.
+        <span className='font-bold'>Hello, I'm Ravindu Aththanayaka, </span>
+        <span>an earnest {" "} <span className='font-bold'>Undergraduate </span>deeply passionate about <span className='font-bold'>Software Development</span>.
         </span>
         </motion.h1>
 
