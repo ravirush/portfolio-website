f8fb044d259b6fc3feb46c391e4a2a4a9009cbf1
Ravindu Aththanayaka <ravindurashmika2k@gmail.com>
2024-05-14 21:13:54 +0530

    intro update

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,8 +65,10 @@ export default function Intro() {
           >
             Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" /></Link>
 
-          <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60" href="/CV.pdf" download>
-            Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" /></a>
+          <a href="/cv.pdf" download className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer 
+          border border-black/10 dark:bg-white/10'>
+          Download CV <HiDownload className='opacity-60 group-hover:translate-x-1 transition'/>
+          </a>
 
           <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/ravinduath/" target="_blank">
