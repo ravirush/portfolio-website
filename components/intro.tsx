92fde7a16e51e3b5968e04cfd4e5bd94ca39308f
Ravindu Aththanayaka <ravindurashmika2k@gmail.com>
2025-01-01 18:58:16 +0530

    resume updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,7 +65,7 @@ export default function Intro() {
           >
             Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" /></Link>
 
-          <a href="/Ravindu_Aththanayaka_CV.pdf" download className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer 
+          <a href="/Ravindu_Aththanayaka_Resume.pdf" download className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer 
           border border-black/10 dark:bg-white/10'>
           Download CV <HiDownload className='opacity-60 group-hover:translate-x-1 transition'/>
           </a>
